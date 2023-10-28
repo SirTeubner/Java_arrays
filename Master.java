57c6dfe885a771e09c69d596e051848d74802c26
Noah Teubner <nteubner@schueler.hakzell.at>
2023-10-29 00:57:00 +0200

    blib blub

--- Master.java
+++ Master.java
@@ -7,11 +7,14 @@ public class Master{
         meinArray = new int[4];
         meinArray[0] = 7;
         meinArray[1] = 12;
+        // auch möglich:
+        // int[] meinArray = int[] {7, 12, 6, 4};
 
 
        System.out.println("Feld 1: " + meinArray[0]);
        System.out.println("Feld 2: " + meinArray[1]);
        System.out.println("Ganzes Array: " + Arrays.toString(meinArray));
+
        
 
     }
